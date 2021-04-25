e2c8b30c893b471dc1956fc3aca3526a147ed7e4
Andrei Dzebysh <presnia@yahoo.com>
2021-04-25 15:24:28 +0300

    Fixed bugs

--- js/players.js
+++ js/players.js
@@ -1,34 +1,20 @@
 import Player from './Player/index.js';
-import { changeHP, elHP, renderHP } from './playerHelpers.js'
+
 
 const player1 = new Player({
   player: 1,
-  changeHP,
-  renderHP,
-  elHP,
   name: 'Scorpion',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
   rootSelector: 'arenas',
-  weapon: ['sabre of light', 'sawblade', 'sword', 'sai', 'sektors rocket'],
-  attack: () => {
-    console.log(this.name + 'Fight...');
-  }
 });
 
 const player2 = new Player({
   player: 2,
-  changeHP,
-  renderHP,
-  elHP,
   name: 'Kitana',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
   rootSelector: 'arenas',
-  weapon: ['kitana', 'sword', 'sai', 'sento', 'saber teeth'],
-  attack: () => {
-    console.log(this.name + 'Fight...');
-  }
 });
 
 export { player1, player2 };
